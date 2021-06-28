7057a141bed88b1a33edec2bd6615ae188c76d5e
Fluke <jesadakorn.kirtnu@gmail.com>
2021-06-28 17:01:04 +0700

    fix fail:

--- pages/docs.js
+++ pages/docs.js
@@ -1,6 +1,5 @@
 import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
-import docs from '../utils/docs.json'
-console.log(docs)
-const App = () => <SwaggerUI url='http://localhost:3000/api/docs' />
+const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT
+const App = () => <SwaggerUI url={`${ENDPOINT}/docs`} />
 export default App
